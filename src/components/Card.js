55e2c03013510c0c1e467eac7f669447e5d0e223
iddqd-alxtor <alexnaq@gmail.com>
2023-05-20 02:17:52 +0300

    final commit

--- src/components/Card.js
+++ src/components/Card.js
@@ -1,7 +1,7 @@
 import React from "react";
-import './card.css';
+import './Card.css';
 
-const Card = ({name , email, id}) => {
+const Card = ({name, email, id}) => {
     return (
         <div className="tc bg-light-green dib br3 pa3 ma2 grow bw2 shadow-5">
             <img src={`https://robohash.org/${id}?50x50`} alt='robots'></img>
